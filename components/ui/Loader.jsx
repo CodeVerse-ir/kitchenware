f5f8be5b9309d5a3214c4048ae389d07fa0b8a2b
mohammad kabiri <codeversesite@gmail.com>
2024-07-22 22:34:48 +0330

    create Loader

---
+++ components/ui/Loader.jsx
@@ -0,0 +1,38 @@
+import './Loader.css';
+
+export default function Loader() {
+    return (
+        <>
+            <div className="background loader-container">
+                <div className="loader-circle"></div>
+                <div className="loader-circle"></div>
+                <div className="loader-circle"></div>
+                <div className="loader-circle"></div>
+            </div>
+
+            {/* <main className="background py-8 md:pt-40 md:pb-20 lg:pt-44 lg:pb-24">
+<div className="container">
+
+    <div className="h-screen bg-white dark:bg-zinc-700 shadow rounded-md p-4 mx-auto">
+        <div className="animate-pulse flex flex-col items-center justify-center gap-y-4">
+            <div className="w-[40%] h-10 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+            <div className="w-[20%] h-5 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+            <div className="w-[70%] h-96 rounded-lg bg-slate-700 dark:bg-slate-300"></div>
+            <div className="flex flex-col w-full items-start justify-center gap-y-4">
+                <div className="w-[10%] h-8 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+                <div className="w-full space-y-2">
+                    <div className="w-full h-5 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+                    <div className="w-full h-5 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+                    <div className="w-full h-5 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+                    <div className="w-full h-5 bg-slate-700 dark:bg-slate-300 rounded-full"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</main> */}
+
+        </>
+    )
+}
